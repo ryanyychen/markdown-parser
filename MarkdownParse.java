b6b15e1723392dd2eab4dc79b35f6f8f201282b6
ryanyychen <ryan_yychen@hotmail.com>
2022-04-28 12:18:45 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,10 +5,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
+        // test-file3.md -- no full sets
+        // test-file4.md -- no full sets
+        // test-file5.md -- should not work for separated )[
+        // test-file7.md -- no full sets
+        // test-file8.md -- has extra [ but nothing else... infiinite loop
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
@@ -22,31 +28,23 @@ public class MarkdownParse {
             currentIndex = closeParen + 1;
 
             if (currentIndex >= markdown.length()) break;
+            if (markdown.indexOf("[", currentIndex) == -1) {
+                break;
+            }
 
             // skip empty lines in the end of file
-            while (Character.toString(markdown.charAt(currentIndex)).equals(System.getProperty("line.separator"))) {
-                currentIndex++;
-                if (currentIndex == markdown.length()) break;
-            }
+            // while (Character.toString(markdown.charAt(currentIndex)).equals(System.getProperty("line.separator"))) {
+            //     currentIndex++;
+            //     if (currentIndex == markdown.length()) break;
+            // }
         }
 
         return toReturn;
     }
 
-    public String read(String fileName) throws IOException {
-        try {
-            Path file = Path.of(fileName);
-            String content = Files.readString(file);
-            return content;
-        } catch (IOException e) {
-            throw new IOException("Invalid file");
-        }
-    }
-
-
     public static void main(String[] args) throws IOException {
         //Path fileName = Path.of(args[0]);
-        Path fileName = Path.of("test-file6.md");
+        Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
