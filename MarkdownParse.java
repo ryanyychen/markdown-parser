9004d87bf509b9b15ab67dfc86a4ae06d83ef613
ryanyychen <ryan_yychen@hotmail.com>
2022-04-28 12:30:04 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,33 +10,31 @@ import java.util.Stack;
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
-        // test-file3.md -- no full sets
-        // test-file4.md -- no full sets
         // test-file5.md -- should not work for separated )[
-        // test-file7.md -- no full sets
-        // test-file8.md -- has extra [ but nothing else... infiinite loop
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         System.out.println("Length: " + markdown.length());
+
+        // search for each of the parentheses/brackets
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+
             currentIndex = closeParen + 1;
 
-            if (currentIndex >= markdown.length()) break;
-            if (markdown.indexOf("[", currentIndex) == -1) {
-                break;
-            }
+            // if any of the parentheses/brackets don't exist
+            if (openBracket == -1 || closeBracket == -1
+                    || openParen == -1 || closeParen == -1) break;
 
-            // skip empty lines in the end of file
-            // while (Character.toString(markdown.charAt(currentIndex)).equals(System.getProperty("line.separator"))) {
-            //     currentIndex++;
-            //     if (currentIndex == markdown.length()) break;
-            // }
+            // if '(' follows directly after ']'
+            if (openParen != closeBracket + 1) continue;
+            else toReturn.add(markdown.substring(openParen + 1, closeParen));
+
+            // end of file
+            if (currentIndex >= markdown.length()) break;
         }
 
         return toReturn;
@@ -44,7 +42,7 @@ public class MarkdownParse {
 
     public static void main(String[] args) throws IOException {
         //Path fileName = Path.of(args[0]);
-        Path fileName = Path.of(args[0]);
+        Path fileName = Path.of("test-file5.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
