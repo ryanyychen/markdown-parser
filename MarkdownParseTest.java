3d98a5e30686e45c26eb6afb7df762bd1565c7f4
ryanyychen <ryan_yychen@hotmail.com>
2022-05-21 14:36:28 -0700

    Snippet Tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -9,217 +10,126 @@ public class MarkdownParseTest {
    private MarkdownParse parser;
    private ArrayList<String> expected;
    private ArrayList<String> URLs;
+   private Path file;
    private String content;
 
    @Before
    public void setup() {
-      //initialize new MarkdownParse object
       parser = new MarkdownParse();
-
       expected = new ArrayList<String>();
       URLs = new ArrayList<String>();
-      content = "empty";
    }
 
    @Test
-   public void testfile1() {
-      //set up expected string from first test file
+   public void testfile1() throws IOException{
       expected.add("https://something.com");
       expected.add("some-thing.html");
 
-      //get contents of first test file
-      try {
-         Path file = Path.of("test-file.md");
-         content = Files.readString(file);
-      } catch (Exception e) {
-         System.out.println("Invalid File");
-      }
-
-      //call getLinks
-      try {
-         URLs = parser.getLinks(content);
-      } catch (Exception e) {
-         System.out.println(URLs);
-         System.out.println(e.getMessage());
-         fail();
-      }
-
-      //check that expected equals actual
+      file = Path.of("test-file.md");
+      content = Files.readString(file);
+      URLs = parser.getLinks(content);
       assertEquals(expected.toString(), URLs.toString());
    }
 
    @Test
-   public void testfile2() {
-      //set up expected string from first test file
+   public void testfile2() throws IOException{
       expected.add("https://something.com");
       expected.add("some-page.html");
 
-      //get contents of first test file
-      try {
-         Path file = Path.of("test-file2.md");
-         content = Files.readString(file);
-      } catch (Exception e) {
-         System.out.println("Invalid File");
-      }
-
-      //call getLinks
-      try {
-         URLs = parser.getLinks(content);
-      } catch (Exception e) {
-         System.out.println(URLs);
-         System.out.println(e.getMessage());
-         fail();
-      }
-
-      //check that expected equals actual
+      file = Path.of("test-file2.md");
+      content = Files.readString(file);
+      URLs = parser.getLinks(content);
       assertEquals(expected.toString(), URLs.toString());
    }
 
    @Test
-   public void testfile3() {
+   public void testfile3() throws IOException{
       //expected should be empty
-      //get contents of first test file
-      try {
-         Path file = Path.of("test-file3.md");
-         content = Files.readString(file);
-      } catch (Exception e) {
-         System.out.println("Invalid File");
-      }
-
-      //call getLinks
-      try {
-         URLs = parser.getLinks(content);
-      } catch (Exception e) {
-         System.out.println(URLs);
-         System.out.println(e.getMessage());
-         fail();
-      }
-
-      //check that expected equals actual
+      file = Path.of("test-file3.md");
+      content = Files.readString(file);
+      URLs = parser.getLinks(content);
       assertEquals(expected.toString(), URLs.toString());
    }
 
    @Test
-   public void testfile4() {
+   public void testfile4() throws IOException {
       //expected should be empty
-      //get contents of first test file
-      try {
-         Path file = Path.of("test-file4.md");
-         content = Files.readString(file);
-      } catch (Exception e) {
-         System.out.println("Invalid File");
-      }
-
-      //call getLinks
-      try {
-         URLs = parser.getLinks(content);
-      } catch (Exception e) {
-         System.out.println(URLs);
-         System.out.println(e.getMessage());
-         fail();
-      }
-
-      //check that expected equals actual
+      file = Path.of("test-file4.md");
+      content = Files.readString(file);
+      URLs = parser.getLinks(content);
       assertEquals(expected.toString(), URLs.toString());
    }
 
    @Test
-   public void testfile5() {
+   public void testfile5() throws IOException{
       //expected should be empty
-      //get contents of first test file
-      try {
-         Path file = Path.of("test-file5.md");
-         content = Files.readString(file);
-      } catch (Exception e) {
-         System.out.println("Invalid File");
-      }
-
-      //call getLinks
-      try {
-         URLs = parser.getLinks(content);
-      } catch (Exception e) {
-         System.out.println(URLs);
-         System.out.println(e.getMessage());
-         fail();
-      }
-
-      //check that expected equals actual
+      file = Path.of("test-file5.md");
+      content = Files.readString(file);
+      URLs = parser.getLinks(content);
       assertEquals(expected.toString(), URLs.toString());
    }
 
    @Test
-   public void testfile6() {
-      //set up expected string from first test file
+   public void testfile6() throws IOException{
       expected.add("page.com");
 
-      //get contents of first test file
-      try {
-         Path file = Path.of("test-file6.md");
-         content = Files.readString(file);
-      } catch (Exception e) {
-         System.out.println("Invalid File");
-      }
-
-      //call getLinks
-      try {
-         URLs = parser.getLinks(content);
-      } catch (Exception e) {
-         System.out.println(URLs);
-         System.out.println(e.getMessage());
-         fail();
-      }
-
-      //check that expected equals actual
+      file = Path.of("test-file6.md");
+      content = Files.readString(file);
+      URLs = parser.getLinks(content);
       assertEquals(expected.toString(), URLs.toString());
    }
 
    @Test
-   public void testfile7() {
+   public void testfile7() throws IOException{
       //expected should be empty
-      //get contents of first test file
-      try {
-         Path file = Path.of("test-file7.md");
-         content = Files.readString(file);
-      } catch (Exception e) {
-         System.out.println("Invalid File");
-      }
-
-      //call getLinks
-      try {
-         URLs = parser.getLinks(content);
-      } catch (Exception e) {
-         System.out.println(URLs);
-         System.out.println(e.getMessage());
-         fail();
-      }
-
-      //check that expected equals actual
+      file = Path.of("test-file7.md");
+      content = Files.readString(file);
+      URLs = parser.getLinks(content);
       assertEquals(expected.toString(), URLs.toString());
    }
 
    @Test
-   public void testfile8() {
-      //set up expected string from first test file
+   public void testfile8() throws IOException{
       expected.add("a link on the first line");
 
-      //get contents of first test file
-      try {
-         Path file = Path.of("test-file8.md");
-         content = Files.readString(file);
-      } catch (Exception e) {
-         System.out.println("Invalid File");
-      }
+      file = Path.of("test-file8.md");
+      content = Files.readString(file);
+      URLs = parser.getLinks(content);
+      assertEquals(expected.toString(), URLs.toString());
+   }
 
-      //call getLinks
-      try {
-         URLs = parser.getLinks(content);
-      } catch (Exception e) {
-         System.out.println(URLs);
-         System.out.println(e.getMessage());
-         fail();
-      }
+   @Test
+   public void testSnippet1() throws IOException {
+      expected.add("'google.com");
+      expected.add("google.com");
+      expected.add("ucsd.edu");
 
-      //check that expected equals actual
+      file = Path.of("snippet1.md");
+      content = Files.readString(file);
+      URLs = parser.getLinks(content);
+      assertEquals(expected.toString(), URLs.toString());
+   }
+
+   @Test
+   public void testSnippet2() throws IOException {
+      expected.add("a.com");
+      expected.add("a.com(())");
+      expected.add("example.com");
+
+      file = Path.of("snippet2.md");
+      content = Files.readString(file);
+      URLs = parser.getLinks(content);
+      assertEquals(expected.toString(), URLs.toString());
+   }
+
+   @Test
+   public void testSnippet3() throws IOException {
+      expected.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+
+      file = Path.of("snippet3.md");
+      content = Files.readString(file);
+      URLs = parser.getLinks(content);
       assertEquals(expected.toString(), URLs.toString());
    }
 }
+
